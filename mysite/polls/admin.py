70d143970d7973e226383a3b59e8004ec1efeb87
Ruth Fong <ruthcfong@gmail.com>
2014-01-05 22:00:00 +0300

    first commit

---
+++ mysite/polls/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from polls.models import Question, Choice
+
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,  {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']})
+    ]
+    inlines = [ChoiceInline]
+
+    # customize the admin change list
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+    list_filter = ['pub_date']
+
+    search_fields = ['question_text']
+
+# Register your models here.
+# admin.site.register(Question)
+
+admin.site.register(Question, QuestionAdmin)
+
+# admin.site.register(Choice)
